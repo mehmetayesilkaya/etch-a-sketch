f523f579075ebd7eac83b9816cf18e1f430f09ba
mehmetayesilkaya <mehmeta.yesilkaya@gmail.com>
2026-04-17 15:37:20 +0300

    feat: implement 16x16 flexbox grid and hover effect

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const container = document.querySelector('#container');
 for (let i = 0; i < 256; i++) {
   const boxes = document.createElement('div');
   boxes.classList.add('box');
-
   container.appendChild(boxes);
+
+  boxes.addEventListener('mouseenter', () => {
+    boxes.style.backgroundColor = 'black';
+  });
 }
