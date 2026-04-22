1d5ce999d2334bf1380cf572c3f9d6241bfd2727
mehmetayesilkaya <mehmeta.yesilkaya@gmail.com>
2026-04-22 12:24:15 +0300

    feat: implement core Etch-a-Sketch functionality

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 const container = document.querySelector('#container');
+const gridBtn = document.querySelector('.gridBtn');
 
-for (let i = 0; i < 256; i++) {
-  const boxes = document.createElement('div');
-  boxes.classList.add('box');
-  container.appendChild(boxes);
+gridBtn.addEventListener('click', () => {
+  const userInput = prompt(
+    'How many grids per side do you want? Please enter a value from 1 to 100'
+  );
 
-  boxes.addEventListener('mouseenter', () => {
-    boxes.style.backgroundColor = 'black';
-  });
+  if (userInput === null) return;
+  if (userInput >= 1 && userInput <= 100) {
+    createGrid(userInput);
+  } else {
+    alert('Please enter a number and this number has to be from 1 to 100');
+  }
+});
+
+function createGrid(value) {
+  container.replaceChildren();
+  for (let i = 0; i < value * value; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width = 100 / value + '%';
+    box.style.height = 100 / value + '%';
+    container.appendChild(box);
+
+    box.addEventListener('mouseenter', () => {
+      box.style.backgroundColor = 'red';
+      box.style.border = 'solid black';
+    });
+  }
 }
+
+createGrid(16);
